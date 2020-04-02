510536d92b250fa2c76b58d9e3dcf1e728e89b77
Nikita Ojamäe <niojam@ttu.ee>
2020-04-02 16:09:41 +0300

    Add Generalization class

---
+++ src/main/java/test/fujitsu/videostore/ui/ModelList.java
@@ -0,0 +1,65 @@
+package test.fujitsu.videostore.ui;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.HasUrlParameter;
+
+public abstract class ModelList<T> extends HorizontalLayout implements HasUrlParameter<String> {
+
+    private TextField filter;
+
+    private Button newItem;
+
+    public HorizontalLayout createTopBar() {
+        TextField filter = createFilter();
+        newItem = new Button();
+        setNewItemTextAndLogic();
+        newItem.setId("new-item");
+        newItem.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        newItem.setIcon(VaadinIcon.PLUS_CIRCLE.create());
+
+        HorizontalLayout topLayout = new HorizontalLayout();
+        topLayout.setWidth("100%");
+        topLayout.add(filter);
+        topLayout.add(newItem);
+        topLayout.setVerticalComponentAlignment(Alignment.START, filter);
+        topLayout.expand(filter);
+        return topLayout;
+    }
+
+
+    public abstract TextField createFilter();
+
+    public Button getNewItemButton() {
+        return newItem;
+    }
+
+    public abstract void setNewItemTextAndLogic();
+
+    public void showSaveNotification(String msg) {
+        Notification.show(msg);
+    }
+
+    public void setNewMovieEnabled(boolean enabled) {
+        newItem.setEnabled(enabled);
+    }
+
+    public VerticalLayout buildVerticalLayout(Grid<T> grid) {
+        HorizontalLayout topLayout = createTopBar();
+        VerticalLayout barAndGridLayout = new VerticalLayout();
+        barAndGridLayout.add(topLayout);
+        barAndGridLayout.add(grid);
+        barAndGridLayout.setFlexGrow(1, grid);
+        barAndGridLayout.setFlexGrow(0, topLayout);
+        barAndGridLayout.setSizeFull();
+        barAndGridLayout.expand(grid);
+        return barAndGridLayout;
+    }
+
+}
